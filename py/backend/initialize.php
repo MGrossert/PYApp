2e77e3f0cd21662cbc7fee3da15f2dc3c9300611
MGRossert <martin.grossert@gmail.com>
2014-08-06 09:57:35 +0200

    ...

---
+++ py/backend/initialize.php
@@ -0,0 +1,17 @@
+<?php
+##############################################
+# PROVIDE HOOKS
+##############################################
+
+Const PY_HOOK_INITIALIZED = "py-initialized";
+$system->registerHook(PY_HOOK_INITIALIZED, "Called after initialized.");
+
+##############################################
+#  
+##############################################
+
+
+// var_export($system->readStructure(true));
+// echo microtime(true)-START;
+// var_dump(opcache_get_configuration());
+// die(PY_HOOK_INITIALIZED);
